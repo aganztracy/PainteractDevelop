53a8840cfa95e7c463dd213f74d6d8b97e9208c8
aganztracy <942179704@qq.com>
2019-04-09 18:56:02 +0800

    add loading page
    add loading page

---
+++ Painteract_3.13/Assets/Scripts/Loading.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Load",1.5f);
+    }
+    void Load(){
+        Debug.Log(":)");
+        SceneManager.LoadScene("MOTA(lo-fi)", LoadSceneMode.Single);
+    }
+
+
+}
